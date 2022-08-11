a20d6b88e91e16b9071b7b62027818a231948d3d
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 12:18:07 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 -  사용자 정의 함수 추가하기

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -1,5 +1,12 @@
 const result = 0;
 let currentResult = result;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('The result is ' +result);
+}
+
+add(1,2);
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${result}+10)* 3 / 2 - 1`;

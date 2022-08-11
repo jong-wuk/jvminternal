fac3435fdef2df8e03f34600617c9c831a3b8779
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 12:03:30 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 -  문자열 더 보기

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -2,8 +2,8 @@ const result = 0;
 let currentResult = result;
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = "("+result+")"+"* 3" +
-    " / 2 -1";
+let calculationDescription = `(${result}+10)* 3 / 2 - 1`;
+let errorMessage = 'An error' + ' occured!';
 
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMessage);

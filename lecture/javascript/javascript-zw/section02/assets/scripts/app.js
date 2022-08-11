bc84d98ab52fe2864b6e06fa2cf787ce6c3ad1fa
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 14:09:11 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 -  숫자 & 문자열 섞기

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -1,15 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add() {
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, '');
 }
 
 
-currentResult = add(1, 2);
-
-let calculationDescription = `(${currentResult}+10)* 3 / 2 - 1`;
+addBtn.addEventListener('click', add);
 
 
-outputResult(currentResult, calculationDescription);

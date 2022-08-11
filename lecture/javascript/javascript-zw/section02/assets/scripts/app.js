97d09d054511f4160a0e7401b9efc69c101bb0cd
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 11:28:53 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 -  상수 사용하기

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -1,9 +1,9 @@
-let result = 0;
-
-result = (result + 10) * 3 / 2 - 1;
+const result = 0;
+let currentResult = result;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = "("+result+")"+"* 3" +
     " / 2 -1";
 
 
-outputResult(result, calculationDescription);
+outputResult(currentResult, calculationDescription);

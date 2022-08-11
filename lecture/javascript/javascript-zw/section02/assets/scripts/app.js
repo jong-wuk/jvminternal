c3704f958c812c71226174e9d6576bd763e7d405
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 11:24:20 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 -  자료형 : 숫자 & 문자열(텍스트)

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -1,5 +1,9 @@
 let result = 0;
 
-result = (result + 10) * 3 / 2 -1;
+result = (result + 10) * 3 / 2 - 1;
 
-outputResult(result, '');
+let calculationDescription = "("+result+")"+"* 3" +
+    " / 2 -1";
+
+
+outputResult(result, calculationDescription);

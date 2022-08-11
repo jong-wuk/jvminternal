6054d14b73087297110e68e656e079863e5722b1
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 13:48:22 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 -  전역 & 로컬 범위 소개

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -1,9 +1,11 @@
-const result = 0;
-let currentResult = result;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 function add(num1, num2) {
-    return num1 + num2;
+    const result = num1 + num2;
+    return result;
 }
+
 
 currentResult = add(1, 2);
 

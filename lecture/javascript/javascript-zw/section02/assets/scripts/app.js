890f22db8ca16cfe2b519c8b9160391e11010c67
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 14:42:24 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 -  모든 버튼들을 함수에 연결하기

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -1,18 +1,47 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function getUserNumberInput(){
+function getUserNumberInput() {
     return parseInt(userInput.value);
 }
 
-function add() {
-    let enteredNumber = getUserNumberInput;
-    const calcDescription = `${currentResult} + ${enteredNumber}`
-    currentResult = currentResult +enteredNumber;
-    outputResult(currentResult,calcDescription);
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+
 }
 
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteLog('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLog('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLog('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLog('/', initialResult, enteredNumber);
+}
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 

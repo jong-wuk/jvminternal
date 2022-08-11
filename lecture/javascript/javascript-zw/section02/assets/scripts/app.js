96d0b96b0e61434ff89c2481bdab0b306841cd88
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-11 15:57:51 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 객체를 사용하는 재사용 가능한 함수 추가하기

--- lecture/javascript/javascript-zw/section02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section02/assets/scripts/app.js
@@ -13,20 +13,25 @@ function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
 
 }
 
+function writeToLog(operationIdentifier, previousResult, operationNumber, newResult) {
+    const logEntry ={
+        operation: operationIdentifier,
+        previousResult: previousResult,
+        number: operationNumber,
+        result: newResult
+
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult += enteredNumber;
     createAndWriteLog('+', initialResult, enteredNumber);
-    const logEntry ={
-        operation: 'ADD',
-        previousResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    };
-    logEntries.push(logEntry);
-    console.log(logEntry.operation);
-    console.log(logEntries);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+
 }
 
 function subtract() {
@@ -34,6 +39,7 @@ function subtract() {
     const initialResult = currentResult;
     currentResult -= enteredNumber;
     createAndWriteLog('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
@@ -41,6 +47,8 @@ function multiply() {
     const initialResult = currentResult;
     currentResult *= enteredNumber;
     createAndWriteLog('*', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+
 }
 
 function divide() {
@@ -48,6 +56,8 @@ function divide() {
     const initialResult = currentResult;
     currentResult /= enteredNumber;
     createAndWriteLog('/', initialResult, enteredNumber);
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+
 }
 
 addBtn.addEventListener('click', add);

677b941f846129a06a012263e33441f764ebd22c
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-12 12:29:24 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - if 문장 이용해 승리 조건 확인하기.

--- lecture/javascript/javascript-zw/section04-02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section04-02/assets/scripts/app.js
@@ -1,13 +1,24 @@
-let chosenMaxLife = 100;
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
+function attackHandler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
-    currentPlayerHealth -= damage;
-
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("몬스터를 물리쳤습니다.!!")
+        resetGame(adjustHealthBars(chosenMaxLife));
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("플레이어가 졌습니다!");
+    }else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert("비겼습니다..!!");
+    }
 }
-attackBtn.addEventListener('click',attackHandler);
+
+attackBtn.addEventListener('click', attackHandler);

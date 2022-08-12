85110d94dfc4eef59dc19a6a5efd46436b75a171
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-12 10:42:07 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - if, else, else-if로 작업하기

---
+++ lecture/javascript/javascript-zw/section04/assets/scripts/app.js
@@ -0,0 +1,77 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function resetLogAndNumber(){
+    logEntries = [];
+    currentResult = 0;
+    outputResult(0,"0");
+}
+
+// 로그를 만들고 출력하는 함수
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+
+}
+
+function writeToLog(operationIdentifier, previousResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier, previousResult: previousResult, number: operationNumber, result: newResult
+
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+function calculateResult(calculationType) {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    let mathOperator;
+    if (calculationType === 'ADD') {
+        currentResult += enteredNumber;
+        mathOperator = '+';
+    } else if (calculationType === 'SUBTRACT') {
+        currentResult -= enteredNumber;
+        mathOperator = '-';
+    } else if (calculationType === 'MULTIPLY') {
+        currentResult *= enteredNumber;
+        mathOperator = '*';
+    } else {
+        currentResult /= enteredNumber;
+        mathOperator = '/';
+    }
+    createAndWriteLog(mathOperator, initialResult, enteredNumber);
+    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+
+}
+
+function add() {
+    calculateResult('ADD');
+}
+
+function subtract() {
+    calculateResult('SUBTRACT');
+}
+
+function multiply() {
+    calculateResult('MULTIPLY');
+
+}
+
+function divide() {
+    calculateResult('DIVIDE');
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+resetBtn.addEventListener('click', resetLogAndNumber);
+
+
+

ab39d4a03be6d1fe62d66e3107030136b4088448
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-17 17:21:22 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 프로젝트 작업하기: 게임에 사용자의 선택 추가하기

--- lecture/javascript/javascript-zw/section06/app.js
+++ lecture/javascript/javascript-zw/section06/app.js
@@ -1,28 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const SCISSORS = "가위";
+const ROCK = "바위";
+const PAPER = "보";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
 
 
-const start = function() {
-    console.log("Game is starting...");
-};
-
-/*const start = function() {
-    console.log("Game is starting...");
-};*/
-/*function start() {
-    console.log("Game is starting...",age);
-}*/
-/*const person = {
-    name:'Max',
-    greet: function greet(){
-        console.log("hello there");
+const getPlayerChoice = function () {
+    const selection = prompt(`${SCISSORS}, ${ROCK}, ${PAPER}?`, '').toUpperCase();
+    if (selection !== SCISSORS
+        && selection !== ROCK
+        && selection !== PAPER)
+    {
+        alert("유효하지 않은 값 입니다! 대신 " + DEFAULT_USER_CHOICE + "를 선택 해드렸습니다!");
+        return DEFAULT_USER_CHOICE;
     }
-};
-
-person.greet();*/
-// console.dir(startGame);
+    return selection;
+        }
 
 startGameBtn.addEventListener("click", function startGame() {
-    console.log("Game is starting...",age);
+    if(gameIsRunning){
+        return;
+    }
+    gameIsRunning = true;
+    console.log("Game is starting...");
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
 });
-startGameBtn.addEventListener("click", start);

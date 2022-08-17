316c315b73b2ccf402b16804bc75c8dad2f1538a
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-17 12:13:13 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - try-catch를 사용해 오류를 잡고 처리하기

--- lecture/javascript/javascript-zw/section04-02/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section04-02/assets/scripts/app.js
@@ -10,13 +10,29 @@ const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
 const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enteredValue = prompt("당신과 몬스터의 최대 체력 수치를 입력해 주세요.", '100');
 
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 let lastLoggedEntry;
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+
+function getMaxLifeValues() {
+    const enteredValue = prompt("당신과 몬스터의 최대 체력 수치를 입력해 주세요.", '100');
+
+    const parsedValue = parseInt(enteredValue);
+    if (isNaN(parsedValue) || parsedValue <= 0) {
+        throw {message: "Invalid user input , not a number!"};
+    }
+    return parsedValue;
+}
+
+let chosenMaxLife;
+try {
+    chosenMaxLife = getMaxLifeValues();
+
+} catch (error) {
+    console.log(error);
     chosenMaxLife = 100;
+    alert("기본값 100으로 재설정 되었습니다.");
+    // throw error;
 }
 
 let currentMonsterHealth = chosenMaxLife;

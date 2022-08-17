b2abe9da3f1ae5b2aa893873aec1732fb8a67c30
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-17 09:26:52 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 코드 게임 로직 구현하기

--- lecture/javascript/javascript-zw/section06/app.js
+++ lecture/javascript/javascript-zw/section06/app.js
@@ -4,6 +4,9 @@ const SCISSORS = "가위";
 const ROCK = "바위";
 const PAPER = "보";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -12,20 +15,49 @@ const getPlayerChoice = function () {
     const selection = prompt(`${SCISSORS}, ${ROCK}, ${PAPER}?`, '').toUpperCase();
     if (selection !== SCISSORS
         && selection !== ROCK
-        && selection !== PAPER)
-    {
+        && selection !== PAPER) {
         alert("유효하지 않은 값 입니다! 대신 " + DEFAULT_USER_CHOICE + "를 선택 해드렸습니다!");
         return DEFAULT_USER_CHOICE;
     }
     return selection;
-        }
+}
+
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+
+const getWinner = function (computerChoice, playerChoice) {
+    if(computerChoice === playerChoice){
+        return RESULT_DRAW;
+    }else if (
+        computerChoice === ROCK && playerChoice === PAPER ||
+        computerChoice === PAPER && playerChoice === SCISSORS ||
+        computerChoice === SCISSORS && playerChoice === ROCK
+    ) {
+        return RESULT_PLAYER_WINS;
+
+    }else{
+        return RESULT_COMPUTER_WINS;
+    }
+
+};
 
 startGameBtn.addEventListener("click", function startGame() {
-    if(gameIsRunning){
+    if (gameIsRunning) {
         return;
     }
     gameIsRunning = true;
     console.log("Game is starting...");
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerChoice)
+    console.log(winner);
 });

4d080d8b4a55f06c6f83969b66d698591fe4abc2
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-17 16:56:25 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 익명 함수

--- lecture/javascript/javascript-zw/section06/app.js
+++ lecture/javascript/javascript-zw/section06/app.js
@@ -1,10 +1,12 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
 
-const start = function() {
+/*const start = function() {
     console.log("Game is starting...");
-};
-
+};*/
+/*function start() {
+    console.log("Game is starting...",age);
+}*/
 /*const person = {
     name:'Max',
     greet: function greet(){
@@ -15,4 +17,6 @@ const start = function() {
 person.greet();*/
 // console.dir(startGame);
 
-startGameBtn.addEventListener("click", start);
+startGameBtn.addEventListener("click", function startGame() {
+    console.log("Game is starting...",age);
+});

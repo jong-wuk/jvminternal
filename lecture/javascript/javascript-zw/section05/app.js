8f9618f0abbfa9c38f041d927cac889b446f2fc2
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-17 16:07:42 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 가비지 콜렉션 & 메모리 관리

--- lecture/javascript/javascript-zw/section05/app.js
+++ lecture/javascript/javascript-zw/section05/app.js
@@ -1,26 +1,21 @@
-/*
-var name = "Max";
-let hobbies;
-if(name === 'Max'){
-  hobbies = ['Sports', 'Cooking'];
-  console.log("inner ",hobbies);
-  if(true){
-    console.log("inner-inner ",hobbies);
-  }
+const addListenerBtn = document.getElementById('add-listener-btn');
+const clickableBtn = document.getElementById('clickable-btn');
+const messageInput = document.getElementById('click-message-input');
+
+let person = {name: 'Wook'};
+
+preson = null;
+
+function printMessage() {
+  const value = messageInput.value;
+  console.log(value || 'Clicked me!');
 }
 
-function greet(){
-  var age = 30;
-  var name = 'Wook';
-  console.log(name, age,hobbies);
+function addListener() {
+  clickableBtn.addEventListener('click', function(){
+    const value = messageInput.value;
+    console.log(value || 'Clicked me!');
+  });
 }
-console.log(name,hobbies);
-greet();
-console.log(name);*/
-"use strict";
 
-let userName = "Max";
-var undefined = 5;
-
-console.log(userName);
-
+addListenerBtn.addEventListener('click', addListener);

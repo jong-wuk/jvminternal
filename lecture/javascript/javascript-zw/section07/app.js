3f9a773cbf201c46fc5bea83f047afe3c085c7b5
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-18 15:54:56 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - DOM 탐색 vs 쿼리 메서드

--- lecture/javascript/javascript-zw/section07/app.js
+++ lecture/javascript/javascript-zw/section07/app.js
@@ -1,17 +1,3 @@
-// const listItemElements = document.querySelectorAll('li')
-const listItemElements = document.getElementsByTagName('li');
-const h1 = document.getElementById("main-title");
-
-// const ul = li.parentElement;
-
-h1.textContent = "새로운 타이틀";
-h1.style.color = "white";
-h1.style.backgroundColor = 'black';
-
-const li = document.querySelector("li:last-of-type");
-li.textContent = li.textContent + "change";
-
-
-for (const listItemEl of listItemElements) {
-    console.dir(listItemEl);
-}
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+console.log(firstLi);

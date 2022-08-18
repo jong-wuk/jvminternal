17a2a6dd5c3477b81821902b17a06c3b0ab4e0e3
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-18 17:27:45 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 모달과 추가 버튼 선택하기

---
+++ lecture/javascript/javascript-zw/section07-01/assets/scripts/app.js
@@ -0,0 +1,4 @@
+const addMovieModal = document.getElementById("add-modal");
+
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;

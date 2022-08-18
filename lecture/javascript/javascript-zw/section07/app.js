2abc17e5e803c09b1328f6eed223863aa4e3130f
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-18 15:17:51 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 다수의 요소 선택하기 & 요약

---
+++ lecture/javascript/javascript-zw/section07/app.js
@@ -0,0 +1,15 @@
+// const listItemElements = document.querySelectorAll('li')
+const listItemElements = document.getElementsByTagName('li');
+const h1 = document.getElementById("main-title");
+
+h1.textContent = "새로운 타이틀";
+h1.style.color = "white";
+h1.style.backgroundColor = 'black';
+
+const li = document.querySelector("li:last-of-type");
+li.textContent = li.textContent + "change";
+
+
+for (const listItemEl of listItemElements) {
+    console.dir(listItemEl);
+}

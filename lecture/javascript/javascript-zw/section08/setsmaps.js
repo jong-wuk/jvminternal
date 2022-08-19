704430bb8ba81b22b35f31b2a12616d12c817a40
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-19 16:23:04 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - Sets 작업하기

---
+++ lecture/javascript/javascript-zw/section08/setsmaps.js
@@ -0,0 +1,9 @@
+// const ids = new Set(['hi','from','set']);
+// ids.add(2);
+// ids.delete('hi');
+// console.log(ids.has(2));
+//
+// const entries = ids.entries();
+// for (const entry of entries) {
+//     console.log(entry);
+// }

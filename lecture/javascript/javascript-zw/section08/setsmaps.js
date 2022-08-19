02d208dbe0d2f76bc5db6fdc97ce55e3178e6631
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-19 16:33:55 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - Maps 작업하기

--- lecture/javascript/javascript-zw/section08/setsmaps.js
+++ lecture/javascript/javascript-zw/section08/setsmaps.js
@@ -7,3 +7,21 @@
 // for (const entry of entries) {
 //     console.log(entry);
 // }
+
+const person1 = {name: "Max"};
+const person2 = {name: "Manuel"};
+
+const personData = new Map([[person1, [{data: 'yesterday', price: 10, product: "Book"}]]]);
+personData.set(person2, [{data:'two weeks ago', price: 100}]);
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const [key,value] of personData.entries()) {
+    console.log(key,value);
+}
+for (const key of personData.keys()) {
+    console.log(key);
+}
+for (const value of personData.values()) {
+    console.log(value);
+}

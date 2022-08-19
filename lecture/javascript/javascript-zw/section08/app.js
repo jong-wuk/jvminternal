fa3a49f0f005c972efaff40ee0ab0051b1518958
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-19 13:55:20 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 반복문 대안 : forEach() 메서드

--- lecture/javascript/javascript-zw/section08/app.js
+++ lecture/javascript/javascript-zw/section08/app.js
@@ -34,26 +34,39 @@ console.log(hobbies);
 hobbies.splice(-1, 1);
 console.log(hobbies);*/
 
+//
+// const testLists = [1, 5.3, 1.5, 10.988, -5, 'a'];
+// // const sliceTestLists = testLists.slice(-2, -1);
+// const sliceTestLists = testLists.concat([3.99, 2, 5]);
+// console.log("sliceTestLists: " + sliceTestLists);
+// testLists.push(10);
+// console.log("testList: " + testLists);
+// console.log(testLists.lastIndexOf('a'));
+// console.log(testLists.includes(1.5));
+//
+// const personData = [{name: 'MAX'}, {name: 'Michael'}];
+// console.log(personData.indexOf({name: 'Michael'}))
+//
+// const michael = personData.find((person) => {
+//     return person.name === 'Michael';
+// });
+// michael.name = "Anna";
+// console.log(michael,personData);
+//
+// const MaxIndex = personData.findIndex((person) => {
+//     return person.name === 'MAX';
+// });
+// console.log(MaxIndex);
+const prices = [10.99, 5.99, 3.23, 6.56];
+const tax = 0.19;
+const taxAdjustedPrices= [];
+// for (const price of prices) {
+//     taxAdjustedPrices.push(price * (1 + tax));
+// }
 
-const testLists = [1, 5.3, 1.5, 10.988, -5, 'a'];
-// const sliceTestLists = testLists.slice(-2, -1);
-const sliceTestLists = testLists.concat([3.99, 2, 5]);
-console.log("sliceTestLists: " + sliceTestLists);
-testLists.push(10);
-console.log("testList: " + testLists);
-console.log(testLists.lastIndexOf('a'));
-console.log(testLists.includes(1.5));
-
-const personData = [{name: 'MAX'}, {name: 'Michael'}];
-console.log(personData.indexOf({name: 'Michael'}))
-
-const michael = personData.find((person) => {
-    return person.name === 'Michael';
+prices.forEach((price,idx,prices) => {
+    const priceObj = {index: idx, taxAdjustedPrices: price * (1 + tax)};
+    taxAdjustedPrices.push(priceObj);
 });
-michael.name = "Anna";
-console.log(michael,personData);
 
-const MaxIndex = personData.findIndex((person) => {
-    return person.name === 'MAX';
-});
-console.log(MaxIndex);
+console.log(taxAdjustedPrices);

d5d83ae03e51172c8d186197083ee97e5c26031e
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-19 14:00:41 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - map()으로 데이터 변환하기

--- lecture/javascript/javascript-zw/section08/app.js
+++ lecture/javascript/javascript-zw/section08/app.js
@@ -57,16 +57,28 @@ console.log(hobbies);*/
 //     return person.name === 'MAX';
 // });
 // console.log(MaxIndex);
+// const prices = [10.99, 5.99, 3.23, 6.56];
+// const tax = 0.19;
+// const taxAdjustedPrices= [];
+// // for (const price of prices) {
+// //     taxAdjustedPrices.push(price * (1 + tax));
+// // }
+//
+// prices.forEach((price,idx,prices) => {
+//     const priceObj = {index: idx, taxAdjustedPrices: price * (1 + tax)};
+//     taxAdjustedPrices.push(priceObj);
+// });
+//
+// console.log(taxAdjustedPrices);
+
+
 const prices = [10.99, 5.99, 3.23, 6.56];
 const tax = 0.19;
-const taxAdjustedPrices= [];
-// for (const price of prices) {
-//     taxAdjustedPrices.push(price * (1 + tax));
-// }
 
-prices.forEach((price,idx,prices) => {
+
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
     const priceObj = {index: idx, taxAdjustedPrices: price * (1 + tax)};
-    taxAdjustedPrices.push(priceObj);
+    return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);

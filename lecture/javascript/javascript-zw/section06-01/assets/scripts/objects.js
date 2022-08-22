66f4c4fb0c88103e24d272d3644ffcf547fdca7f
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-22 09:25:50 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 프로퍼티 유형 & 프로퍼티 순서

--- lecture/javascript/javascript-zw/section06-01/assets/scripts/objects.js
+++ lecture/javascript/javascript-zw/section06-01/assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById('movie-list');
 movieList.style['background-color'] = 'red';
 movieList.style.display = 'block';
 
+const userChosenKeyName = 'level';
+
 let person = {
   'first name': 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  [userChosenKeyName]:'a',
   greet: function() {
     alert('Hi there!');
   },
@@ -21,6 +24,11 @@ delete person.age;
 // person.age = null;
 person.isAdmin = true;
 
-console.log(person['first name']);
-console.log(person[1.5]);
+const keyName = 'first name';
+const hello = 1.5;
+const greet = 'greet';
+console.log(person[keyName]);
+console.log(person[hello]);
+console.log(person['level']);
 console.log(person);
+console.log(person[greet]);

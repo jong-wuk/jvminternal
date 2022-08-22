8fa0e549f39cbc6823d47b0787a553869fbe0379
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-22 17:19:15 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 이벤트 리스닝을 하는 다양한 방법

---
+++ lecture/javascript/javascript-zw/section14/assets/scripts/events.js
@@ -0,0 +1,23 @@
+const button = document.querySelector("button");
+
+// button.onclick = function (){
+//
+// }
+
+/*
+const buttonClickHandler = () => {
+    alert('Button was Clicked!');
+    button.style.background = "black";
+};
+
+const anotherButtonClickHandler = ()=>{
+    console.log('clicked!');
+};
+
+
+button.onclick = anotherButtonClickHandler;
+button.onclick = buttonClickHandler;*/
+
+button.addEventListener();
+
+button.removeEventListener();

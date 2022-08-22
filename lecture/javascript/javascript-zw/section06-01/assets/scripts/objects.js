8edc2f32f0ab03e757ae71f80ba3607c1d06f688
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-22 15:18:15 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 마무리

--- lecture/javascript/javascript-zw/section06-01/assets/scripts/objects.js
+++ lecture/javascript/javascript-zw/section06-01/assets/scripts/objects.js
@@ -28,7 +28,7 @@ const renderMovies = (filter = '') => {
         // const {title: movieTitle} = info;
         let text = getFormattedTitle.call(movie) + ' - ';
         for (const key in info) {
-            if (key !== 'title') {
+            if (key !== 'title' && key !== '_title') {
                 text = text + `${key}: ${info[key]}`;
             }
         }
@@ -48,7 +48,16 @@ const addMovieHandler = () => {
 
     const newMovie = {
         info: {
-            title,
+            set title(val) {
+                if (val.trim() == '') {
+                    this._title = 'DEFAULT';
+                    return;
+                }
+                this._title = val;
+            },
+            get title() {
+                return this._title;
+            },
             [extraName]: extraValue
         },
         id: Math.random().toString(),
@@ -58,7 +67,9 @@ const addMovieHandler = () => {
         }
     };
 
+    newMovie.info.title = title;
     movies.push(newMovie);
+    console.log(newMovie.info.title);
     renderMovies();
 };
 

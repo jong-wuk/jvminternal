9715fc9c4dd935377c57a685eb48c6fab3bb829c
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-23 11:48:25 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 태그된 템플릿

--- lecture/javascript/javascript-zw/section16/app.js
+++ lecture/javascript/javascript-zw/section16/app.js
@@ -7,3 +7,20 @@ console.log(randomintBetween(1, 10));
 
 
 // 'hello'.toUpperCase()
+
+function productDescription(strings, productName, productPrice) {
+    console.log(strings);
+    console.log(productName);
+    console.log(productPrice);
+    let priceCategory = 'cheap';
+    if(productPrice > 20){
+        priceCategory = 'fair';
+    }
+    // return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+    return {name: productName, price: productPrice};
+}
+const prodName = 'JavaScript';
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}`;
+console.log(productOutput);

7b812bdade411cad574bd0cf2a91104401906ea1
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-23 15:38:14 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - HTTP 요청 프로미스화하기

--- lecture/javascript/javascript-zw/section18/assets/scripts/app.js
+++ lecture/javascript/javascript-zw/section18/assets/scripts/app.js
@@ -1,14 +1,28 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
-const xhr = new XMLHttpRequest();
 
-xhr.open("GET","https://jsonplaceholder.typicode.com/posts");
+function sendHttpRequest(method, url) {
+    const promise = new Promise((resolve, reject) => {
 
-xhr.responseType = "json";
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
 
-xhr.onload = function () {
-    // const listOfPosts =JSON.parse(xhr.response);
-    const listOfPosts =xhr.response;
+        xhr.responseType = "json";
+
+        xhr.onload = function () {
+            resolve(xhr.response);
+            // const listOfPosts =JSON.parse(xhr.response);
+
+        };
+
+        xhr.send();
+    });
+    return promise;
+}
+
+async function fetchPosts(){
+    const responseData = await sendHttpRequest("GET","https://jsonplaceholder.typicode.com/posts/");
+    const listOfPosts = responseData;
     console.log(listOfPosts);
     for (const post of listOfPosts) {
         const postEl = document.importNode(postTemplate.content, true);
@@ -16,8 +30,10 @@ xhr.onload = function () {
         postEl.querySelector("p").textContent = post.body;
         listElement.append(postEl);
     }
-};
+}
 
-xhr.send();
+fetchPosts();
+
+
 
 

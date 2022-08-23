348e8bff0e274672a9196be3d6501f3997cb8225
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-23 17:42:54 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - IndexedDB 시작하기

--- lecture/javascript/javascript-zw/section22/app.js
+++ lecture/javascript/javascript-zw/section22/app.js
@@ -1,6 +1,20 @@
 const storeButton = document.getElementById('store-btn');
 const retrieveButton = document.getElementById('retrieve-btn');
 
+const dbRequest = indexedDB.open('StorageDummy',1);
+dbRequest.onupgradeneeded = function (event){
+    const db = event.target.result;
+
+    const objStore = db.createObjectStore('products',{keyPath: 'id'});
+
+    objStore.transaction.oncomplete = function (event){
+        const productsStore = db.transaction('products', 'readwrite').objectStore('products');
+        productsStore.add({id:'p1', title:'A First Product', price:12.99, tags: ['Expensive', 'Luxury']});
+    }
+};
+dbRequest.onerror = function (event){
+    console.log('ERROR!');};
+
 
 storeButton.addEventListener('click', () => {
     const userId = 'user12';

8fab90e8763ed658661a6f8591f6939834c0c245
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-23 14:20:35 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 프로미스 오류 처리하기

--- lecture/javascript/javascript-zw/section17/app.js
+++ lecture/javascript/javascript-zw/section17/app.js
@@ -2,35 +2,38 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 const getPosition = (opts) => {
-    const promise = new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(success =>{
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(success => {
             resolve(success);
-        }, error=>{
-
-        },opts)
+        }, error => {
+            reject(error);
+        }, opts)
 
     });
-    return promise;
 }
 
 const setTimer = (duration) => {
-    const promise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve('Done!');
         }, duration);
     });
-    return promise;
 };
 
 function trackUserHandler() {
     let positionData;
-    getPosition().then(posData=>{
-        positionData = posData;
-        return setTimer(2000);
-    }).then(data=>{
-        console.log(data,positionData);
+    getPosition()
+        .then(posData => {
+            positionData = posData;
+            return setTimer(2000);
+        }).catch(err => {
+        console.log(err);
+        console.log("on we go");
     })
-    setTimer(1000).then(()=>{
+        .then(data => {
+            console.log(data, positionData);
+        })
+    setTimer(1000).then(() => {
         console.log("Timer done..!");
     })
 

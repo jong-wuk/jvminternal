cf2e09d8625deba8f21dc7830cf9d20936387787
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-23 14:01:43 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - 프로미스 시작하기

--- lecture/javascript/javascript-zw/section17/app.js
+++ lecture/javascript/javascript-zw/section17/app.js
@@ -1,13 +1,29 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, duration);
+    });
+    return promise;
+};
+
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(posdData =>{
-    console.log(posdData);
-  },error=>{
-    console.log(error);
-  });
-  console.log('Getting Position....');
+    navigator.geolocation.getCurrentPosition(posData => {
+            setTimer(2000).then(data => {
+                console.log(data, posData);
+            });
+        }, error => {
+            console.log(error);
+        }
+    );
+    setTimer(1000).then(()=>{
+        console.log("Timer done..!");
+    })
+
+    console.log('Getting Position....');
 }
 
 button.addEventListener('click', trackUserHandler);

00b9e99dc63f52a0226b73510dbfb43b0cff2791
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-23 17:50:41 +0900

    JavaScript 완벽 가이드 : 초급 + 고급 마스터 과정 - IndexedDB로 작업하기

--- lecture/javascript/javascript-zw/section22/app.js
+++ lecture/javascript/javascript-zw/section22/app.js
@@ -1,20 +1,35 @@
 const storeButton = document.getElementById('store-btn');
 const retrieveButton = document.getElementById('retrieve-btn');
 
+let db;
+
 const dbRequest = indexedDB.open('StorageDummy',1);
+dbRequest.onsuccess = function (event){
+     db = event.target.result;
+
+};
+
 dbRequest.onupgradeneeded = function (event){
-    const db = event.target.result;
+     db = event.target.result;
 
     const objStore = db.createObjectStore('products',{keyPath: 'id'});
 
     objStore.transaction.oncomplete = function (event){
         const productsStore = db.transaction('products', 'readwrite').objectStore('products');
-        productsStore.add({id:'p1', title:'A First Product', price:12.99, tags: ['Expensive', 'Luxury']});
+        productsStore.add({id:'p2', title:'A First Product', price:12.99, tags: ['Expensive', 'Luxury']});
     }
 };
 dbRequest.onerror = function (event){
     console.log('ERROR!');};
 
+retrieveButton.addEventListener('click', () => {
+    const productsStore = db.transaction('products', 'readwrite').objectStore('products');
+    const request = productsStore.get('p2');
+
+    request.onsuccess = function(){
+        console.log(request.result);
+    }
+});
 
 storeButton.addEventListener('click', () => {
     const userId = 'user12';

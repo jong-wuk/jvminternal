3957a5ba54d27ef03e302e0d05ee5f8109737ec9
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-24 17:22:13 +0900

    계산기 입력 - 버튼 클릭시, 화면에 직접 입력시, 입력 화면 구현

--- handOn/calculator/app.js
+++ handOn/calculator/app.js
@@ -1,24 +1,27 @@
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator__keys");
-const display = document.querySelector(".calculator__display");
-let logEntries=[];
-let currentResult = 0;
-keys.addEventListener("click",(e)=>{
-    const key = e.target;
-    const action = key.dataset.action;
-    const keyContent = key.textContent;
-    const displayedNum = display.textContent;
-    if(!action){
-        if (displayedNum === '0') {
-            display.textContent = keyContent;
-            logEntries.push(keyContent);
-        }else{
-            display.textContent = displayedNum + keyContent;
-             logEntries.push(keyContent);
-        }
-    }else if(action ==='clear'){
-        logEntries = [];
-        currentResult = 0;
-        display.textContent = '0';
+const userInput = document.getElementById("input-text");
+const clearBtn = document.getElementById("btn-clear");
+const buttons = document.getElementById("calc-actions");
+const currentResultOutput = document.getElementById("current-result");
+const currentCalculationOutput = document.getElementById("current-calculation");
+function outputResult(result, text){
+    currentResultOutput.textContent = result;
+    currentCalculationOutput.textContent = text;
+}
+
+buttons.addEventListener('click', function(event){
+    const target = event.target;
+    const action = target.classList[0];
+    const buttonContent = target.textContent;
+    if(action === "number"){
+        userInput.valueOf().value += buttonContent;
     }
 })
+
+function clearLogAndNumber() {
+    userInput.valueOf().value = "";
+    outputResult(0, "0");
+}
+
+clearBtn.addEventListener('click',clearLogAndNumber);
+
+

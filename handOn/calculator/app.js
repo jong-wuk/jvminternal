f0ad739197c6405e7a4a113cfb92736203eb536c
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-24 15:24:38 +0900

    계산기 clear 기능 구현

--- handOn/calculator/app.js
+++ handOn/calculator/app.js
@@ -1,3 +1,24 @@
-const outPut = document.getElementById("output");
-const button1 = document.getElementById("")
-outPut.addEventListener()
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+let logEntries=[];
+let currentResult = 0;
+keys.addEventListener("click",(e)=>{
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    if(!action){
+        if (displayedNum === '0') {
+            display.textContent = keyContent;
+            logEntries.push(keyContent);
+        }else{
+            display.textContent = displayedNum + keyContent;
+             logEntries.push(keyContent);
+        }
+    }else if(action ==='clear'){
+        logEntries = [];
+        currentResult = 0;
+        display.textContent = '0';
+    }
+})

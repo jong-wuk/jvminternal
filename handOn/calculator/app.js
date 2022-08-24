155a483d8751683cb60bfea10227de2f2807a524
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-24 18:46:57 +0900

    계산기 화면 - 키보드 이벤트로 입력화면 구현

--- handOn/calculator/app.js
+++ handOn/calculator/app.js
@@ -3,25 +3,31 @@ const clearBtn = document.getElementById("btn-clear");
 const buttons = document.getElementById("calc-actions");
 const currentResultOutput = document.getElementById("current-result");
 const currentCalculationOutput = document.getElementById("current-calculation");
-function outputResult(result, text){
+
+function outputResult(result, text) {
     currentResultOutput.textContent = result;
     currentCalculationOutput.textContent = text;
 }
 
-buttons.addEventListener('click', function(event){
+buttons.addEventListener('click', function (event) {
     const target = event.target;
     const action = target.classList[0];
     const buttonContent = target.textContent;
-    if(action === "number"){
+    if (action === "number") {
         userInput.valueOf().value += buttonContent;
     }
 })
 
-function clearLogAndNumber() {
-    userInput.valueOf().value = "";
-    outputResult(0, "0");
-}
+window.addEventListener("keydown", (ev) => {
 
-clearBtn.addEventListener('click',clearLogAndNumber);
-
+    if (ev.key >= "0" && ev.key <= "9") {
+        userInput.valueOf().value += ev.key;
+    } else if (ev.key === "Enter") {
+        currentResultOutput.textContent = eval(userInput.valueOf().value);
+    } else if (ev.key === "+" || ev.key === "-" || ev.key === "*" || ev.key === "/") {
+        userInput.valueOf().value += ev.key;
+    } else if (ev.key >= "a" && ev.key <= "z") {
+        ev.preventDefault();
+    }
+})
 

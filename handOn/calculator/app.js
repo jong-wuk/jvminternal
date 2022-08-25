ab36912445cbf0f22ec774975d4738834fe523d4
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-25 11:31:54 +0900

    계산기 히스토리(모달ver) - 값 출력 전까지.

--- handOn/calculator/app.js
+++ handOn/calculator/app.js
@@ -1,23 +1,65 @@
 const userInput = document.getElementById("input-text");
-const clearBtn = document.getElementById("btn-clear");
 const buttons = document.getElementById("calc-actions");
 const currentResultOutput = document.getElementById("current-result");
 const currentCalculationOutput = document.getElementById("current-calculation");
+const logButton = document.querySelector(".iconBtn");
+const modal = document.getElementById("LogModal");
+const closeSpan = document.getElementsByClassName("close")[0];
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logResult = [];
+function resetNumber(){
+    currentResult = 0;
+    outputResult(0,"0");
+    userInput.value = '';
+}
+
+function createLogResult(logResult,logDescription){
+    const element = document.createElement("div");
+    element.innerHTML = `
+        <p>${logResult}</p>
+        <p>${logDescription}</p>
+    `;
+
+}
+function printLogResult(){
+    logResult.forEach(log=>{
+        createLogResult(log,"");
+    })
+}
 
 function outputResult(result, text) {
     currentResultOutput.textContent = result;
     currentCalculationOutput.textContent = text;
 }
 
+logButton.addEventListener('click',function(){
+    //클릭시 팝업
+    modal.style.display = "block";
+    printLogResult();
+})
+
+closeSpan.addEventListener('click',function(){
+    modal.style.display = "none";
+})
+
+window.addEventListener("click", function (event){
+    if(event.target === modal){
+        modal.style.display = "none";
+    }
+})
+
 buttons.addEventListener('click', function (event) {
     const target = event.target;
     const btnText = target.textContent;
     const buttonContent = target.textContent;
     if(btnText ==="="){
-        currentResultOutput.textContent = eval(userInput.valueOf().value);
+        currentResult +=  eval(userInput.valueOf().value);
+        currentResultOutput.textContent =currentResult;
+        outputResult(eval(userInput.valueOf().value),userInput.valueOf().value);
+        logResult.push(currentResult);
     }else if(btnText ==="CLEAR"){
-        userInput.valueOf().value = "";
-        currentResultOutput.textContent = "";
+        resetNumber();
     }
     else{
         userInput.valueOf().value += buttonContent;
@@ -27,14 +69,15 @@ buttons.addEventListener('click', function (event) {
 
 window.addEventListener("keydown", (ev) => {
 
+
     if (ev.key >= "0" && ev.key <= "9") {
         userInput.valueOf().value += ev.key;
     } else if (ev.key === "Enter") {
-        currentResultOutput.textContent = eval(userInput.valueOf().value);
+        outputResult(eval(userInput.valueOf().value),userInput.valueOf().value);
     } else if (ev.key === "+" || ev.key === "-" || ev.key === "*" || ev.key === "/") {
         userInput.valueOf().value += ev.key;
     } else if (ev.key >= "a" && ev.key <= "z") {
-        ev.preventDefault();
+        ev.stopPropagation();
     }
 })
 

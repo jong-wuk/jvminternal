fa0070fe95a82776c80c188e5ddae66f117e52bc
jong_wuk <jongwuk.hwang@howoocast.com>
2022-08-25 16:03:14 +0900

    계산기 히스토리(모달ver) - 히스토리 기능, 스크롤 처리 완료

--- handOn/calculator/app.js
+++ handOn/calculator/app.js
@@ -1,50 +1,20 @@
 const userInput = document.getElementById("input-text");
 const buttons = document.getElementById("calc-actions");
 const currentResultOutput = document.getElementById("current-result");
-const currentCalculationOutput = document.getElementById("current-calculation");
-const logButton = document.querySelector(".iconBtn");
 const modal = document.getElementById("LogModal");
-const closeSpan = document.getElementsByClassName("close")[0];
 const defaultResult = 0;
 let currentResult = defaultResult;
 let logResult = [];
-function resetNumber(){
+
+function resetNumber() {
     currentResult = 0;
-    outputResult(0,"0");
+    currentResultOutput.value = 0;
     userInput.value = '';
+    logResult = [];
 }
 
-function createLogResult(logResult,logDescription){
-    const element = document.createElement("div");
-    element.innerHTML = `
-        <p>${logResult}</p>
-        <p>${logDescription}</p>
-    `;
-
-}
-function printLogResult(){
-    logResult.forEach(log=>{
-        createLogResult(log,"");
-    })
-}
-
-function outputResult(result, text) {
-    currentResultOutput.textContent = result;
-    currentCalculationOutput.textContent = text;
-}
-
-logButton.addEventListener('click',function(){
-    //클릭시 팝업
-    modal.style.display = "block";
-    printLogResult();
-})
-
-closeSpan.addEventListener('click',function(){
-    modal.style.display = "none";
-})
-
-window.addEventListener("click", function (event){
-    if(event.target === modal){
+window.addEventListener("click", function (event) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 })
@@ -53,15 +23,15 @@ buttons.addEventListener('click', function (event) {
     const target = event.target;
     const btnText = target.textContent;
     const buttonContent = target.textContent;
-    if(btnText ==="="){
-        currentResult +=  eval(userInput.valueOf().value);
-        currentResultOutput.textContent =currentResult;
-        outputResult(eval(userInput.valueOf().value),userInput.valueOf().value);
+    if (btnText === "=") {
+        currentResult = eval(userInput.valueOf().value);
+        currentResultOutput.textContent = currentResult;
+        userInput.value += "\r\n";
         logResult.push(currentResult);
-    }else if(btnText ==="CLEAR"){
+        userInput.scrollTop = userInput.scrollHeight;
+    } else if (btnText === "CLEAR") {
         resetNumber();
-    }
-    else{
+    } else {
         userInput.valueOf().value += buttonContent;
     }
 
@@ -73,11 +43,16 @@ window.addEventListener("keydown", (ev) => {
     if (ev.key >= "0" && ev.key <= "9") {
         userInput.valueOf().value += ev.key;
     } else if (ev.key === "Enter") {
-        outputResult(eval(userInput.valueOf().value),userInput.valueOf().value);
+        currentResult = eval(userInput.valueOf().value);
+        currentResultOutput.textContent = currentResult;
+        userInput.value += "\r\n";
+        userInput.scrollTop = userInput.scrollHeight;
     } else if (ev.key === "+" || ev.key === "-" || ev.key === "*" || ev.key === "/") {
         userInput.valueOf().value += ev.key;
     } else if (ev.key >= "a" && ev.key <= "z") {
         ev.stopPropagation();
     }
 })
+
+
 

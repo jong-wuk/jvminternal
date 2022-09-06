4df1c384ae8463a1d2df8efdca6bcc0c5d3c3d50
aorri2 <aorri96@naver.com>
2022-09-06 20:24:44 +0900

    react calculator 프로젝트 리빌딩

--- handOn/my-calculator/src/component/Button.js
+++ handOn/my-calculator/src/component/Button.js
@@ -1,23 +1,87 @@
-function getStyleName(btn) {
+import {useContext} from "react";
+import {CalcContext} from "../context/CalcContext";
+
+const getStyleName = btn => {
 
     const className = {
-        '=':'equals',
-        '*':'opt',
-        '-':'opt',
-        '+':'plus',
-        '/':'opt',
+        '=': 'equals',
+        '*': 'opt',
+        '-': 'opt',
+        '+': 'plus',
+        '/': 'opt',
     }
     return className[btn];
 }
 
 
-const Button = ({value}) =>{
-    function handleBtnClick() {
-        const results = {
+const Button = ({value}) => {
+    const {calc, setCalc} = useContext(CalcContext);
 
+    const clearClick = () => {
+        setCalc({sign: '', num: 0, res: 0})
+        console.log("reset!")
+    }
+
+    const handleClickButton = () => {
+        const numberString = value.toString();
+        let numberValue;
+
+        if (numberString === '0' && calc.num === 0) {
+            numberValue = "0";
+        } else {
+            numberValue = Number(calc.num + numberString);
+        }
+
+        setCalc({
+            ...calc,
+            num: numberValue
+        })
+    }
+    const signClick = () => {
+        setCalc({
+            sign: value,
+            res: !calc.res && calc.num ? calc.num : calc.res,
+            num: 0
+        })
+    }
+
+
+    const equalsClick = () => {
+        if (calc.res && calc.num) {
+            const math = (res, num, sign) => {
+                const result = {
+                    "+": (res, num) => res + num,
+                    "-": (res, num) => res - num,
+                    "*": (res, num) => res * num,
+                    "/": (res, num) => res / num,
+                }
+                return result[sign](res, num);
+            }
+
+        setCalc({
+            res: math(calc.res, calc.num, calc.sign),
+            sign: '',
+            num: 0
+        })
         }
     }
-    return(
+
+    const handleBtnClick = () => {
+        const results = {
+            "CLEAR": clearClick,
+            "/": signClick,
+            "+": signClick,
+            "*": signClick,
+            "-": signClick,
+            "=": equalsClick,
+        }
+        if (results[value]) {
+            return results[value]()
+        } else {
+            return handleClickButton();
+        }
+    }
+    return (
         <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>{value}</button>
     )
 }

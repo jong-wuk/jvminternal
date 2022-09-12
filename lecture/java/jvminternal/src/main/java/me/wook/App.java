7f957ce52d1356f92c3b7c524f4cd96172c5399b
aorri2 <aorri96@naver.com>
2022-09-12 22:23:40 +0900

    클래스 로더

--- lecture/java/jvminternal/src/main/java/me/wook/App.java
+++ lecture/java/jvminternal/src/main/java/me/wook/App.java
@@ -1,11 +1,14 @@
 package me.wook;
 
+
 public class App {
-        static int age = 10;
-        static{
-            age = 20;
-            System.out.println("static block");
-        }
+    static int age = 10;
+
+    static {
+        age = 20;
+        System.out.println("static block");
+    }
+
     public static void main(String[] args) {
         ClassLoader classLoader = App.class.getClassLoader();
         System.out.println(classLoader);
